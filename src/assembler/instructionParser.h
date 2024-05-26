900e62a20eb10872e8e79c9a9e22c3b32719b923
Charbs <bx223@ic.ac.uk>
2024-05-26 16:42:35 +0100

    altered helper functions in header file

--- src/assembler/instructionParser.h
+++ src/assembler/instructionParser.h
@@ -3,32 +3,10 @@
 extern int *instructionParser(char *instruction, int sizeIns);
 // takes a single instruction and parse it into corresponding binary code
 
-int branchParser(char *instruction, int sizeIns);
-// helper. Parses branch instructions into binary
+int multParser(char *instruction, int sizeIns);
 
-int aritParser(char *instruction, int sizeIns);
-// helper. Parses arithmetic instructions into binary
+int twoOprParser(char *instruction, int sizeIns);
 
-int cmpParser(char *instruction, int sizeIns);
-// helper. Parses compare instructions into binary
+int uniOprParser(char *instruction, int sizeIns);
 
-int logicInsParser(char *instruction, int sizeIns);
-// helper. Parses logic instructions into binary
-
-int tstParser(char *instruction, int sizeIns);
-// helper. Parses test bits instruction into binary using logicInsParser
-
-int mvnTransParser(char *instruction, int sizeIns);
-// helper. Parses mvn instruction into binary using logicInsParser
-
-int movParser(char *instruction, int sizeIns);
-// helper. Parses mov instruction into binary
-
-int wMovParser(char *instruction, int sizeIns);
-// helper. Parses wide move instructions into binary
-
-int m4Parser(char *instruction, int sizeIns);
-// helper. Parses madd and msub instructions into binary
-
-int dataTransParser(char *instruction, int sizeIns);
-// helper. Parses str and ldr instructions into binary
+int twoOprNoDesParser(char *instruction, int sizeIns);
