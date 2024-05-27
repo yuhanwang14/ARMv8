f7ad90210c2ca1d192f3310ba62a8903a8647a49
bx223 <bx223@ic.ac.uk>
2024-05-27 15:34:29 +0100

    ref: TwoOprParser

--- src/assembler/instructionParser.c
+++ src/assembler/instructionParser.c
@@ -1,5 +1,28 @@
 #include "instructionParser.h"
 
+
+
+const uint8_t OPC_ADD = 0;
+const uint8_t OPC_ADDS = 1;
+const uint8_t OPC_AND = 0;
+const uint8_t OPC_ANDS = 3;
+const uint8_t OPC_BIC = 0;
+const uint8_t OPC_BICS = 3;
+const uint8_t OPC_EOR = 2;
+const uint8_t OPC_EON = 2;
+const uint8_t OPC_ORR = 1;
+const uint8_t OPC_ORN = 1;
+const uint8_t OPC_SUB = 2;
+const uint8_t OPC_SUBS = 3;
+const uint8_t DPI_IMM_ARITHMETIC_23_28 = 33;
+const uint8_t DPI_REG_ARITH_LOGIC_25_28 = 5;
+const uint8_t REGISTER_ADR_SIZE = 5;
+
+void appendBits(uint32_t *target,uint32_t appended,uint32_t length){
+    *target = ((*target) << length) + appended;
+    return;
+}
+
 int instructionParser(char *instruction, int sizeString) {
 
 }
@@ -9,133 +32,75 @@ int twoOprParser(char *instruction, int sizeString) {
     char opcode[5];
     strcpy(opcode, strtok_r(instruction, " ", buffer));
     char *arguments[4];
-    char opc[4];
-    char negate = '0'; // holds the value for N
+    int8_t opc;
+    int8_t negate = 0; // holds the value for N
     bool logicOp = false;
-    char result[33];
+    uint32_t result = 0;
     for (int i = 0; i < 4; i++) {
         arguments[i] = strtok_r(NULL, " ", buffer);
     }
-    switch (opcode[0]) {
-        case 'a':
-            if (opcode[1] == 'd') {
-                // add or adds
-                if (opcode[3] == 's') {
-                    // adds
-                    strcpy(opc, "01");
-                } else {
-                    // add
-                    strcpy(opc, "00");
-                }
-            } else {
-                // and or ands
-                logicOp = true;
-                if (opcode[3] == 's') {
-                    // ands
-                    strcpy(opc, "11");
-                } else {
-                    // and
-                    strcpy(opc, "00");
-                }
-            }
-            break;
-        case 'b':
-            // bic/bics
-            logicOp = true;
-            negate = '1';
-            if (opcode[3] == 's') {
-                //bics
-                strcpy(opc, "11");
-            } else {
-                //bic
-                strcpy(opc, "00");
-            }
-            break;
-        case 'e':
-            // eor/eon
-            logicOp = true;
-            strcpy(opc, "10");
-            if (opcode[2] == 'n') {
-                // eon
-                negate = '1';
-            }
-            break;
-
-        case 'o':
-            // orr/orn
-            logicOp = true;
-            strcpy(opc, "01");
-            if (opcode[2] == 'n')
-                negate = '1'; // orn
-            break;
-
-        case 's':
-            // sub/subs
-            if (opcode[3] == 's') {
-                // adds
-                strcpy(opc, "11");
-            } else {
-                // add
-                strcpy(opc, "10");
-            }
-            break;
-
-        default:
-            printf("could not parse %s\nerror occur at twoOprParser, opcode fails to parse", instruction);
-            return -1;
+    if (strcmp(opcode,"add") == 0){
+        opc = OPC_ADD;
+    }else if (strcmp(opcode,"adds") == 0){
+        opc = OPC_ADDS;
+    }else if (strcmp(opcode,"sub") == 0){
+        opc = OPC_SUB;
+    }else if (strcmp(opcode,"subs") == 0){
+        opc = OPC_SUBS;
+    }else if (strcmp(opcode,"and") == 0){
+        opc = OPC_AND;
+        logicOp = true;
+    }else if (strcmp(opcode,"ands") == 0){
+        opc = OPC_ANDS;
+        logicOp = true;
+    }else if (strcmp(opcode,"bic") == 0){
+        opc = OPC_BIC;
+        logicOp = true;
+        negate = 1;
+    }else if (strcmp(opcode,"bics") == 0){
+        opc = OPC_BICS;
+        logicOp = true;
+        negate = 1;
+    }else if (strcmp(opcode,"eor") == 0){
+        opc = OPC_EOR;
+        logicOp = true;
+    }else if (strcmp(opcode,"eon") == 0){
+        opc = OPC_EON;
+        logicOp = true;
+        negate = 1;
+    }else if (strcmp(opcode,"orr") == 0){
+        opc = OPC_ORR;
+        logicOp = true;
+    }else if (strcmp(opcode,"orn") == 0){
+        opc = OPC_ORN;
+        logicOp = true;
+        negate = 1;
+    }else{
+        fprintf(stderr,"failed to parse opcode for\n '%s'",instruction);
+        exit(EXIT_FAILURE);
     }
-    result[0] = '1';
-    strcpy(&result[1], opc);
+    appendBits(&result,(arguments[0][0] == 'x'),1);
+    appendBits(&result,opc,2);
     if (isImmediate(arguments[2])) {
-        strcpy(&result[3], "100010"); //23
-        strcpy(&result[9], immediate12Parser(arguments[2], arguments[3]));//10,next index 22
-        strcpy(&result[22], fetchRegister(arguments[1]));
-        strcpy(&result[27], fetchRegister(arguments[0]));
+        appendBits(&result,DPI_IMM_ARITHMETIC_23_28,6); //23
+        appendBits(&result, immediate12Parser(arguments[2],arguments[3]),13);//10,next index 22
+        appendBits(&result, registerParser(arguments[1]),REGISTER_ADR_SIZE);
+        appendBits(&result, registerParser(arguments[0]),REGISTER_ADR_SIZE);
     } else {
-        strcpy(&result[3], "0101");
+        appendBits(&result,DPI_REG_ARITH_LOGIC_25_28,4);
         if (logicOp) {
-            result[7] = '0';
-            result[10] = negate;
+            appendBits(&result,0,1);
         } else {
-            result[7] = '1';
-            result[10] = '0';
+            appendBits(&result,1,1);
         }
-        if (arguments[3] == NULL) {
-            result[8] = '0';
-            result[9] = '0';
-        } else {
-            switch (arguments[3][0]) {
-                case 'l':
-                    if (arguments[3][2] == 'l') {
-                        // lsl
-                        strcpy(&result[8], "00");
-                    } else {
-                        // lsr
-                        strcpy(&result[8], "01");
-                    }
-                    break;
-                case 'a':
-                    strcpy(&result[8], "10"); // asr
-                    break;
-                case 'r':
-                    strcpy(&result[8], "11"); // ror
-                    break;
-                default:
-                    printf("could not parse %s\nerror occur at twoOprParser, shift fails to parse", instruction);
-                    return -1;
-            }
-            result[10] = negate;
-            strcpy(&result[11], fetchRegister(arguments[2]));
-            if (arguments[3] == NULL){
-                strcpy(&result[16],"000000");
-            }else{
-                char *temp = strtok(arguments[3]," ");
-                temp = strtok(NULL," ");
-                strcpy(&result[16], immediate6Parser(temp));
-            }
-            strcpy(&result[22], fetchRegister(arguments[1]));
-            strcpy(&result[27], fetchRegister(arguments[0]));
+        uint8_t *parsedShift = shiftParser(arguments[3]);
+        appendBits(&result,parsedShift[0],2);
+        appendBits(&result,negate,1);
+        appendBits(&result, registerParser(arguments[2]),REGISTER_ADR_SIZE);
+        appendBits(&result,parsedShift[1],6);
+        appendBits(&result, registerParser(arguments[1]),REGISTER_ADR_SIZE);
+        appendBits(&result, registerParser(arguments[0]),REGISTER_ADR_SIZE);
         }
+    return result;
     }
-    return flatten(result, 32);
-}
+
--- src/assembler/instructionParser.h
+++ src/assembler/instructionParser.h
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 extern int instructionParser(char *instruction, int sizeIns);
 // takes a single instruction and parse it into corresponding binary code
@@ -13,12 +15,18 @@ int uniOprParser(char *instruction, int sizeIns);
 
 int twoOprNoDesParser(char *instruction, int sizeIns);
 
-int flatten(char *targets, int sizeArray);
-
-char *fetchRegister(char *registerName);
+int8_t registerParser(char *registerName);
 
 bool isImmediate(char *target);
+//checks whether an argument is an instance of immediate
 
-char *immediate12Parser(char *immediateValue, char *shift);
+int16_t immediate12Parser(char *immediateValue, char *shift);
+// Parse 12 bit immediate values with possible shift, returns a 13-bit int
 
-char *immediate6Parser(char *immediateValue);
+int8_t immediate6Parser(char *immediateValue);
+// Parse 6-bit immediate value
+
+uint8_t *shiftParser(char *shiftArg);
+// Parse a shift argument
+// returns an Array of size 2, first elem is the code of shift(2-bit), second elem is the shift value
+// returns {0,0} if passed in NULL
