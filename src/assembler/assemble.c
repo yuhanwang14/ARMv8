b213be11afd6a132c25bd804b3ac1877645e242a
吳子豪 <henrywzh@dyn3140-13.wlan.ic.ac.uk>
2024-05-31 16:58:55 +0100

    feat: assembler main function

--- src/assembler/assemble.c
+++ src/assembler/assemble.c
@@ -1,19 +1,46 @@
 #include <stdlib.h>
 #include "two_pass.h"
+#include "parser.h"
+
+static FILE *out, *source;
+
+FILE *safe_open(char *path, const char *mode) {
+    FILE *f = fopen(path, mode);
+    if (f == NULL) {
+        fprintf(stderr, "Failed to open %s with mode %s", path, mode);
+        exit(EXIT_FAILURE);
+    }
+    return f;
+}
 
 int main(int argc, char **argv) {
-    // FILE *fptr;
-    // FileLines *file_lines = two_pass(fptr);
-    // for (int i = 0; i < file_lines->length; i++) {
-    //     uint32_t encode = parse_instruction(file_lines->lines[i]);
-    //     char *str;
-    //     fprintf(fptr, "%d", encode);
-    // }
+    if (argc == 2) {
+        // print to stdout
+        out = stdout;
+    } else if (argc == 3) {
+        // print to given file
+        out = safe_open(argv[2], "w+");
+    } else {
+        puts("usage: emulate <binary> <log file>");
+        return EXIT_FAILURE;
+    }
+
+    source = safe_open(argv[1], "r");
+
+    FileLines *file_lines = two_pass(source);
+
+    for (int i = 0; i < file_lines->length; i++) {
+        uint32_t result = parse_instruction(file_lines->lines[i], i);
+        fprintf(source, "%d", result);
+    }
+
+    fclose(out);
+    fclose(source);
 
     // debugging
 
 //    char *f1 = "/Users/henrywzh/Desktop/C/armv8_testsuite/test/test_cases/general/dummy_ldr.s";
-    char *f1 = "/Users/henrywzh/Desktop/C/armv8_testsuite/test/test_cases/general/ldr10.s";
+//    char *f1 = "/Users/henrywzh/Desktop/C/armv8_testsuite/test/test_cases/general/ldr10.s";
 //    char *f1 = "/Users/henrywzh/Desktop/C/armv8_testsuite/test/test_cases/branch/b2.s";
 //    char *f1 = "/Users/henrywzh/Desktop/C/armv8_testsuite/test/test_cases/general/example.s";
 
@@ -30,10 +57,10 @@ int main(int argc, char **argv) {
 //    int check = f1_lines[4][len - 1] == ':';
 //    printf("%d\n", check);
 
-    FileLines *f_l = two_pass(f1);
-    for (int i = 0; i < f_l->length; i++) {
-        printf("%d: %s\n", i, f_l->lines[i]);
-    }
+//    FileLines *f_l = two_pass(f1);
+//    for (int i = 0; i < f_l->length; i++) {
+//        printf("%d: %s\n", i, f_l->lines[i]);
+//    }
 
 //    FirstPass *first_pass = split_lines(f1);
 //    for (int i = 0; i < first_pass->length; i++) {
