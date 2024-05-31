691e3a4eff93a8124462f8efba149bc64a4a1100
xiaoshihou514 <onelessononeexercise@gmail.com>
2024-05-31 17:57:41 +0100

    ref!(fetch.c): ram into byte indexed

--- src/emulator/fetch.c
+++ src/emulator/fetch.c
@@ -6,11 +6,11 @@
 #include <stdlib.h>
 
 uint32_t fetch(Register *reg) {
-    if (reg->PC >= WORD_COUNT) {
+    if (reg->PC >= BYTE_COUNT) {
         // reading outside of PC, this is no good
         fprintf(stderr, "\n");
         fprintf(stderr, "PC out of bound\n");
-        fprintf(stderr, "Expected PC to be less than 0x%x, but is 0x%lx\n", WORD_COUNT, reg->PC);
+        fprintf(stderr, "Expected PC to be less than 0x%x, but is 0x%lx\n", BYTE_COUNT, reg->PC);
         exit(EXIT_FAILURE);
     }
 
