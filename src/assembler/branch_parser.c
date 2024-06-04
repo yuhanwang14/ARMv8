2c53d3840cdd66206a6a2a40339095f4d0f9059f
bx223 <bx223@ic.ac.uk>
2024-06-04 11:08:58 +0100

    ref: abstraction of branch_parser

--- src/assembler/branch_parser.c
+++ src/assembler/branch_parser.c
@@ -1,4 +1,5 @@
 #include "branch_parser.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,10 +14,13 @@ static const uint8_t COND_CODE_LT = 11;
 static const uint8_t COND_CODE_GT = 12;
 static const uint8_t COND_CODE_LE = 13;
 static const uint8_t COND_CODE_AL = 14;
+static const uint8_t COND_CODE_SIZE = 4;
+static const uint8_t SIMM19_SIZE = 19;
+static const uint8_t SIMM26_SIZE = 26;
 
 static uint32_t parse_uncond(char *opcode, char **arguments, int32_t currentLoc) {
     uint32_t result = PARSE_UNCOND_START;                                  // pos 26-31
-    bit_append(&result, parse_imm_general(arguments[0]) - currentLoc, 26); // simm 26, pos 0 - 25
+    bit_append(&result, parse_imm_general(arguments[0]) - currentLoc, SIMM26_SIZE); // simm 26, pos 0 - 25
     // offset calculated here
     return result;
 }
@@ -30,22 +34,22 @@ static uint32_t parse_with_reg(char *opcode, char **arguments, int32_t currentLo
 
 static uint32_t parse_cond(char *opcode, char **arguments, int32_t currentLoc) {
     uint32_t result = PARSE_COND_START;                                    // pos 24-31
-    bit_append(&result, parse_imm_general(arguments[0]) - currentLoc, 19); // simm19, pos 5 - 23
+    bit_append(&result, parse_imm_general(arguments[0]) - currentLoc, SIMM19_SIZE); // simm19, pos 5 - 23
     bit_append(&result, 0, 1);                                             // pos 4
     if (strcmp(opcode, "b.eq") == 0) {                                     // cond, pos 0 - 4
-        bit_append(&result, COND_CODE_EQ, 4);
+        bit_append(&result, COND_CODE_EQ, COND_CODE_SIZE);
     } else if (strcmp(opcode, "b.ne") == 0) {
-        bit_append(&result, COND_CODE_NE, 4);
+        bit_append(&result, COND_CODE_NE, COND_CODE_SIZE);
     } else if (strcmp(opcode, "b.ge") == 0) {
-        bit_append(&result, COND_CODE_GE, 4);
+        bit_append(&result, COND_CODE_GE, COND_CODE_SIZE);
     } else if (strcmp(opcode, "b.lt") == 0) {
-        bit_append(&result, COND_CODE_LT, 4);
+        bit_append(&result, COND_CODE_LT, COND_CODE_SIZE);
     } else if (strcmp(opcode, "b.gt") == 0) {
-        bit_append(&result, COND_CODE_GT, 4);
+        bit_append(&result, COND_CODE_GT, COND_CODE_SIZE);
     } else if (strcmp(opcode, "b.le") == 0) {
-        bit_append(&result, COND_CODE_LE, 4);
+        bit_append(&result, COND_CODE_LE, COND_CODE_SIZE);
     } else if (strcmp(opcode, "b.al") == 0) {
-        bit_append(&result, COND_CODE_AL, 4);
+        bit_append(&result, COND_CODE_AL, COND_CODE_SIZE);
     } else {
         fprintf(stderr, "failed to parse the condition of '%s'\n", opcode);
         exit(EXIT_FAILURE);
