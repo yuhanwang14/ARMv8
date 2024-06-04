20449df265f60808a41e53379f1b55a3e6ac1c60
bx223 <bx223@ic.ac.uk>
2024-06-04 12:03:32 +0100

    ref: abstaction on dt_parser

--- src/assembler/dt_parser.c
+++ src/assembler/dt_parser.c
@@ -1,19 +1,23 @@
 #include "dt_parser.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static const int8_t SDT_LITERAL = 24;
-static const int8_t SDT_NOT_LITERAL_25_29 = 28;
+static const uint8_t SDT_LITERAL = 24;
+static const uint8_t SDT_NOT_LITERAL_25_29 = 28;
+static const uint8_t OFFSET_UNSIGNED_SIZE = 12;
+static const uint8_t SF_SIZE = 1;
+static const uint8_t SDT_LITERAL_SIZE = 6;
+static const uint8_t SIMM19_SIZE = 19;
+static const uint8_t SIMM9_SIZE = 9;
+static const uint8_t SDT_REG_10_15 = 26;
 
-static const unsigned SF_SIZE = 1;
-static const unsigned SDT_LITERAL_SIZE = 6;
-static const unsigned SIMM19_SIZE = 19;
 
 static int32_t parse_simm19(char *absoluteAddress, uint32_t currentLoc) {
     int32_t offset = strtol(absoluteAddress + 1, NULL, 0) - currentLoc;
     // performs sign-extension for negative offsets
-    return offset > 0 ? offset : offset + (1 << 19);
+    return offset > 0 ? offset : offset + (1 << SIMM19_SIZE);
 }
 
 static uint32_t parse_dt_literal(char *rt, char *immediate, uint32_t currentLoc) {
@@ -39,7 +43,7 @@ static uint32_t parse_indexed_or_reg(char *opcode, char **addressArg, int8_t num
         // pre/post indexed
         printf("dt_instruction parsed as indexed\n");
         bit_append(&result, 0, 1);                                // pos 21
-        bit_append(&result, parse_imm_general(addressArg[1]), 9); // simm 9, pos 12-20
+        bit_append(&result, parse_imm_general(addressArg[1]), SIMM9_SIZE); // simm 9, pos 12-20
         if (numArg == 3) {
             // pre-indexed
             bit_append(&result, 1, 1); // 'I', pos 11
@@ -52,7 +56,7 @@ static uint32_t parse_indexed_or_reg(char *opcode, char **addressArg, int8_t num
         printf("dt_instruction parsed as register\n");
         bit_append(&result, 1, 1);                                             // pos 21
         bit_append(&result, parse_register(addressArg[1]), REGISTER_ADR_SIZE); // xm, pos 16-20
-        bit_append(&result, 26, 6);                                            // pos 10-15
+        bit_append(&result, SDT_REG_10_15, 6);                                            // pos 10-15
     }
     bit_append(&result, parse_register(addressArg[0]), REGISTER_ADR_SIZE); // xn, pos 5-9
     printf("%i\n", result);
@@ -71,12 +75,12 @@ static uint32_t parse_unsigned(char *opcode, char **addressArg, int8_t numArg, b
     }
     if (numArg == 1) {
         // no immediate supplied
-        bit_append(&result, 0, 12); // offset, pos 10-21
+        bit_append(&result, 0, OFFSET_UNSIGNED_SIZE); // offset, pos 10-21
     } else {
         if (is64) {
-            bit_append(&result, parse_imm_general(addressArg[1]) / 8, 12);
+            bit_append(&result, parse_imm_general(addressArg[1]) / 8, OFFSET_UNSIGNED_SIZE);
         } else {
-            bit_append(&result, parse_imm_general(addressArg[1]) / 4, 12);
+            bit_append(&result, parse_imm_general(addressArg[1]) / 4, OFFSET_UNSIGNED_SIZE);
         }
     }
     bit_append(&result, parse_register(addressArg[0]), REGISTER_ADR_SIZE); // xn, pos 5-9
@@ -86,15 +90,18 @@ static uint32_t parse_unsigned(char *opcode, char **addressArg, int8_t numArg, b
 uint32_t parse_sdt(char *opcode, char *argument, uint32_t currentLoc) {
     char *rt = strtok(argument, " ,");
     char *rest = strtok(NULL, "");
-    if (*rest == ' ')
+    if (*rest == ' ') {
         rest++;
-    if (*rest == '#')
+    }
+    if (*rest == '#') {
         // the second argument is an immediate value, instruction parsed as dt_literal
         return parse_dt_literal(rt, rest, currentLoc);
-    while (*rest == ' ')
+    }
+    while (*rest == ' ') {
         rest++; // removes all spaces at the front(if any)
-    rest = rest +
-           1; // if we reach here, it implies the original str starts with '[', remove the bracket
+    }
+    rest = rest + 1;
+    // if we reach here, it implies the original str starts with '[', remove the bracket
     bool postIndex = rest[3] == ']' ||
                      rest[2] == ']'; // this is used to distinguish Unsigned offset and post-index
     char *nextArg = strtok(rest, ",] ");
