ac52da3ebac65f85c9bd3b427ce0c8421cd1a391
xiaoshihou514 <onelessononeexercise@gmail.com>
2024-06-06 15:20:21 +0100

    doc: two pass

--- src/assembler/two_pass.h
+++ src/assembler/two_pass.h
@@ -3,16 +3,21 @@ typedef struct {
     int value; // corresponding offset
 } Pair;
 
+// an array based map
 typedef struct {
     Pair **pairs;
     int used;
     int size;
 } Map;
 
+// sets a local variable so we don't have to pass it every time
 void set_input(char *path);
 
+// counts the lines and collects the labels
 unsigned first_pass();
 
+// split lines and read into the given buffer
 void read_into(char **buf);
 
+// substitute all the labels into literals
 void substitute_labels(char **lines);
