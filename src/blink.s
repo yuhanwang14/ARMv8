e0f313bc6dc4da1eded8a0c7de28cf66cd297733
xiaoshihou514 <onelessononeexercise@gmail.com>
2024-06-06 21:06:24 +0100

    feat: bling bling bling led

--- src/blink.s
+++ src/blink.s
@@ -1,18 +1,38 @@
-movz x1, #0xf
-movk x4, #0x40
-movz x2, #0xffff, lsl #32
+; set signal
+movz w1, #0x4
+; set output mode
+movk w4, #0x40
+; loop end
+movz x2, #0xf, lsl #16
+; GPIO "register" address
 movz x3, #0x3f20, lsl #16
 
-str x4, [x3]
+; set GPIO to out mode
+str w4, [x3]
 
 start:
-str x1, [x3, #0x001e]
+; set high signal
+str w1, [x3, #0x001c]
 
+; reset counter
 movz x0, #0
-wait:
+
+; do some addition to buy some time
+wait1:
 add x0, x0, #1
 cmp x0, x2
-b.ne wait
+b.ne wait1
 
-str x1, [x3, #0x002a]
+; set low signal
+str w1, [x3, #0x0028]
+
+; reset counter
+movz x0, #0
+
+; do some addition to buy some time
+wait2:
+add x0, x0, #1
+cmp x0, x2
+b.ne wait2
+
 b start
